86aae28a4a50a0093d4c478b7966b7726b879818
Robert Armitage <robert.c.armitage@gmail.com>
2019-12-18 23:50:05 +0000

    updated sql_runner.rb to run on windows with PostgreSQL download, trying to show team name in Homepage league table but not currently working

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'sports_scoring_app', host: 'localhost' })
+      db = PG.connect({ dbname: 'sports_scoring_app', host: 'localhost', user: 'postgres', password: 'Gl55sql' })
       db.prepare( "query", sql )
       result = db.exec_prepared( "query", values )
     ensure
